f4132845b4c5729aad732bb8f71d11d9ccf89ffa
Panna Porri <72212920+pannaporri@users.noreply.github.com>
2021-10-27 01:17:17 +0530

    Create code3.cpp
    help me with the function code

---
+++ code3.cpp
@@ -0,0 +1,17 @@
+// C++ program to print reverse of a string
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to reverse a string
+void reverse(string str)
+{
+
+}
+
+// Driver code
+int main(void)
+{
+	string s = "GeeksforGeeks";
+	reverse(s);
+	return (0);
+}
